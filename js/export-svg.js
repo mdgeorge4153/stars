b284f80825bf859e99038bd7f2fd6d5fed2269d1
Michael George <mdgeorge@cs.cornell.edu>
2016-04-16 13:56:47 -0400

    added functino to output svg files

---
+++ js/export-svg.js
@@ -0,0 +1,26 @@
+
+/**
+ * input: a DOM node containing an SVG image
+ * output: an href containing the serialized SVG.  For example:
+ *   document.getElementById("link").href = svgAsLink(svg);
+ */
+function svgAsLink(svg) {
+  //get svg source.
+  var serializer = new XMLSerializer();
+  var source = serializer.serializeToString(svg);
+
+  //add name spaces.
+  if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+      source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+  }
+  if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+      source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+  }
+
+  //add xml declaration
+  source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+  //convert svg source to URI data scheme.
+  return "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+}
+
